254584822a24746c94563a1c7eea6ad5075d2eda
Михаил Костенчук <MichaelKostekchuk@MacBookMK-7.local>
2010-05-31 19:48:49 +0400

    kill Shop

--- 4sem/luck-va/Shop/app/helpers/providings_helper.rb
+++
@@ -1,6 +0,0 @@
-module ProvidingsHelper
-  def provider_select(name, default = nil)
-    ps = Provider.find(:all, :order => 'name').map{ |p| [p.name, p.id.to_s] }
-    select_tag(name, options_for_select(ps, default))
-  end
-end
